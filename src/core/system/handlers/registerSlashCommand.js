a3394e8e5a4d4d8ae4c925a72978c0084b7a11a6
lowqualityrkomi <75586015+lowqualityrkomi@users.noreply.github.com>
2024-04-16 15:57:54 +0200

    fix: count commands in log

--- src/core/system/handlers/registerSlashCommand.js
+++ src/core/system/handlers/registerSlashCommand.js
@@ -1,31 +1,35 @@
-import { REST, Routes } from "discord.js";
+import "dotenv/config";
+import { Routes } from "discord-api-types/v10";
+import { REST } from "@discordjs/rest";
 import loadCommands from "./loadCommands.js";
 import config from "../../../config.json" assert { type: "json" };
 import "colors";
 
 async function registerSlashCommand(client) {
-  console.log("Refreshing (/) commands".yellow);
+	console.log("Refreshing (/) commands".yellow);
 
-  //Registering the commands
-  const rest = new REST({ version: "10" }).setToken(process.env.token);
-  const commands = await loadCommands(client);
+	//Registering the commands
+	const rest = new REST({ version: "10" }).setToken(process.env.token);
+	const commands = await loadCommands(client);
 
-  try {
-    let setup;
-    if (config.commandSetup.globalCommands) {
-      setup = Routes.applicationCommands(process.env.botId)
-    } else {
-      setup = Routes.applicationGuildCommands(process.env.botId, config.commandSetup.guildId)
-    }
+	try {
+		let guilds = [];
+		if (config.commandSetup.globalCommands) {
+			guilds = client.guilds.cache.map((guild) => guild.id);
+		} else {
+			guilds = [config.commandSetup.guildId];
+		}
 
-    //Command data goes here
-    await rest.put(setup, { body: commands });
+		for (let i = 0; i < guilds.length; i++) {
+			//Command data goes here
+			await rest.put(Routes.applicationGuildCommands(`${process.env.botId}`, `${guilds[i]}`), { body: commands });
+		}
 
-    console.log("Successfully reloaded (/) commands".green);
-  } catch (error) {
-    console.log("Failed to refresh (/) commands".red);
-    console.error(error);
-  }
+		console.log(`Successfully reloaded ${commands.length} (/) commands`.green);
+	} catch (error) {
+		console.log(`Failed to refresh ${commands.length} (/) commands`.red);
+		console.error(error);
+	}
 }
 
 export default registerSlashCommand;
